7c7fcec18ecd05a4c4e19f9362799186d6488aaf
vickeryRev <vickery20091991@gmail.com>
2022-07-12 16:54:42 -0500

    built out login component

--- rps_101_ultimate_showdown/src/app/components/login/login.component.ts
+++ rps_101_ultimate_showdown/src/app/components/login/login.component.ts
@@ -1,3 +1,5 @@
+import { AppComponent } from './../../app.component';
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +7,50 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  username: string = "";
+  password: string = "";
+  loginErrorMsg: string = "";
+  isLoading: boolean = false;
 
-  ngOnInit(): void {
+  constructor(private AuthService: AuthService, private AppComponent: AppComponent) { }
+
+  //pass username and password for template, and call auth service on the back end
+  
+  login(){
+    //check if login values are empty
+
+    if(!this.username.trim() || !this.password.trim()){
+      this.loginErrorMsg = "Login for user failed, must fill out username and password feilds"
+      return;
+    }
+
+    //call auther service, hit /login endpoint on back end
+
+    this.isLoading = true;
+    this.AuthService.login(this.username, this.password)
+    .subscribe(
+      (response) => {
+        this.isLoading = false;
+
+        //build token
+        const token = response.headers.get('rps-ultimate-showdown-token')
+        //save token to session on browser
+        sessionStorage.setItem("token", token)
+
+        this.AppComponent.isLoggedIn = true;
+        this.AppComponent.updateUserData(response.body.username)
+      },
+      //failed login
+      () => {
+        this.isLoading = false;
+        this.loginErrorMsg = ""
+      }
+    );
+    this.username ="";
+    this.loginErrorMsg="";
   }
+  
 
 }
