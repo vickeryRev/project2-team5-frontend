d6444038adea57bf942210c7efc8819a5893c27d
vickeryRev <vickery20091991@gmail.com>
2022-07-12 15:03:19 -0500

    added usermodel - todo: add other classes and data holders

---
+++ rps_101_ultimate_showdown/src/app/Models/user.ts
@@ -0,0 +1,25 @@
+export class User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    username: string;
+    password: string;
+    email: string;
+
+  constructor(
+    id: number, 
+    firstName: string, 
+    lastName: string, 
+    username: string, 
+    password: string, 
+    email: string
+) {
+    this.id = id
+    this.firstName = firstName
+    this.lastName = lastName
+    this.username = username
+    this.password = password
+    this.email = email
+  }
+
+}
