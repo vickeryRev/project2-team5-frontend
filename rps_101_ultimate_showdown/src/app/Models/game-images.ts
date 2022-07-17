9b160113d9c75ab90fdd3e3ccb3aefaa26650115
Jheflin1995 <106621400+Jheflin1995@users.noreply.github.com>
2022-07-17 15:33:02 -0500

    Update game-images.ts

--- rps_101_ultimate_showdown/src/app/Models/game-images.ts
+++ rps_101_ultimate_showdown/src/app/Models/game-images.ts
@@ -1,14 +1,19 @@
 export class GameImages {
 
-    constructor(playerUrl: string, compObjectUrl: string) {
+    constructor(playerUrl: string, compObjectUrl: string, playerName: string, computerName : string, winOrLose : string) {
       this.playerUrl = playerUrl
       this.compObjectUrl = compObjectUrl
+      this.playerName = playerName;
+      this.computerName = computerName;
+      this.winOrLose = winOrLose;
     }
   
   
       playerUrl : string;
       compObjectUrl : string;
-  
+      playerName : string;
+      computerName : string;
+      winOrLose : string;
       
   
   }
